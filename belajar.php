296249a090d0ca8e3735fc7a8ded07e1d0ba1177
Dessy1412 <dessyfitriyanidewi14@gmail.com>
2021-03-02 09:27:32 +0700

    belajar php dasar

---
+++ belajar.php
@@ -0,0 +1,26 @@
+<?php
+
+    echo "Define ini pake variabel cons";
+    echo "</br>";
+    define("PHI", 3.14);
+    $jari_jari = 7;
+    $luas_lingkaran = PHI * $jari_jari * $jari_jari;
+    echo $luas_lingkaran;
+
+    $angka = 1;
+    $nama = "Dessy";
+    $angka_desimal = 3.5;
+    $nama_lengkap = 'Dessy Fitriyani Dewi';
+
+    echo $angka;
+    echo "</br>";
+    echo $nama;
+    echo "</br>";
+    echo $angka_desimal;
+    echo "<h1>Assalamualaikum</h1>";
+    echo "dokumen saya : ".$_SERVER["DOCUMENT_ROOT"];
+    echo "</br>";
+    echo $nama_lengkap;
+    echo "</br>";
+
+?>
